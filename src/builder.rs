3336636251c6b887bf89ab0bec4c4f63873eced1
Arvin Morawej <arvin.morawej@gmail.com>
2023-08-25 17:18:09 -0400

    Simulates a buy on new shares!

---
+++ src/builder.rs
@@ -0,0 +1,73 @@
+use std::ops::{Sub, Mul, Add, Div};
+
+use ethers::{types::{
+    U256, H160, Bytes, Eip1559TransactionRequest
+}, utils::{to_checksum, parse_ether}};
+
+use crate::credentials::my_address;
+
+pub fn build_buy_transaction(
+    buy_from: H160,
+    amount: U256,
+    supply: U256,
+) -> Eip1559TransactionRequest {
+    let data = format!("0x6945b123{:0>64}{:0>64}", &to_checksum(&buy_from, Some(1))[2..], amount);
+    let value = calculate_summation(supply, amount);
+    
+    Eip1559TransactionRequest::default()
+        .from(my_address().parse::<H160>().unwrap())
+        .to("0xcf205808ed36593aa40a44f10c7f7c2f67d4a4d4".parse::<H160>().unwrap())
+        .value(value)
+        .data(data.parse::<Bytes>().unwrap())
+}
+
+fn calculate_summation(supply: U256, amount: U256) -> U256 {
+    let sum1 = if supply == U256::zero() {
+        U256::zero()
+    } else {
+        supply.sub(1).mul(supply).mul(U256::from(2).mul(supply.sub(1)).add(1)).div(6)
+    };
+
+    let sum2 = if supply == U256::zero() && amount == U256::one() {
+        U256::zero()
+    } else {
+        supply.sub(1).add(amount).mul(supply.add(amount)).mul(U256::from(2).mul(supply.sub(1).add(amount)).add(1)).div(6)
+    };
+
+    let summation = sum2 - sum1;
+    let price = summation.mul(parse_ether("1").unwrap()).div(16000);
+    let fee = price.mul(50000000000000000u64).div(parse_ether("1").unwrap());
+    println!("Price {:#?}", price);
+    println!("Fee {:#?}", fee);
+    price.add(fee).add(fee)
+}
+
+pub fn is_new_share (
+    info: (H160, H160, bool, U256, U256, U256, U256, U256)
+) -> bool {
+    if info.0 == info.1 && info.3 == U256::one() && info.7 == U256::one() && info.2 {
+        return true
+    } 
+
+    false
+}
+
+#[cfg(test)]
+mod test_case {
+    use super::*;
+
+    #[test]
+    fn get_fee() {
+        let result = calculate_summation(U256::one(), U256::from(3));
+        println!("Result {:#?}\n", result);
+
+        assert_ne!(result, U256::zero());
+    }
+    #[test]
+    fn get_fee2() {
+        let result = calculate_summation(U256::from(4), U256::one());
+        println!("Result {:#?}", result);
+
+        assert_ne!(result, U256::zero());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use dotenv::dotenv;
 use eyre::Result;
-use ethers::{providers::{Http, Provider,Ws, Middleware, StreamExt}, types::{BlockNumber, H160, Filter}};
+use ethers::{providers::{Http, Provider,Ws, Middleware, StreamExt}, types::{BlockNumber, H160, Filter, U256, U64}};
 
 mod credentials;
-mod decoder;
 use credentials::*;
+mod decoder;
 use decoder::*;
+mod builder;
+use builder::*;
 
 #[tokio::main]
 async fn main() -> Result<()>{
@@ -17,15 +19,41 @@ async fn main() -> Result<()>{
     let ws_provider: Provider<Ws> = Provider::<Ws>::connect(ws_provider_url()).await?;
     // Getting pending transactions from mempool
     let mut pending_transactions_stream = ws_provider.subscribe_logs(&Filter::new().select(BlockNumber::Latest).event("Trade(address,address,bool,uint256,uint256,uint256,uint256,uint256)")).await?;
+    // Getting Latest blocks for mined TRXs
+    let mut latest_block_stream = ws_provider.subscribe_blocks().await?;
+
+    // Variables
+    let mut block_number = U64::zero();
+    let mut nonce = U256::zero();
+    let mut base_fee = U256::zero();
 
     let result = provider.get_balance(my_address().parse::<H160>().unwrap(), Some(BlockNumber::Latest.into())).await;
     println!("Bal {:#?}", result);
+
     loop {
         tokio::select! {
+            Some(latest_block) = latest_block_stream.next() => {
+
+                // Get Mined Blocks
+                block_number = latest_block.number.unwrap();
+                nonce = provider.get_transaction_count(my_address().parse::<H160>().unwrap(), Some(BlockNumber::Latest.into())).await?;
+                base_fee = latest_block.next_block_base_fee().unwrap();
+                println!("{} {}", "New nonce:", nonce);
+                println!("{} {}", "New Block:", block_number);
+                println!("{} {}", "New fee:", base_fee);
+
+            },
+
             Some(pending_transaction) = pending_transactions_stream.next() => {
 
-                println!("PT {:#?}", pending_transaction)
+                let results = decode_buy_share(pending_transaction.data);
 
+                if is_new_share(results) {
+                    let buy_transaction = build_buy_transaction(results.1, U256::one(), results.7);
+                    let result2 = provider.call(&buy_transaction.into(), Some(block_number.into())).await;
+                    println!("PT {:#?} \n {:#?}", results, result2)
+                }
+                
             },
 
             else => {
