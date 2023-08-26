01c724fe04402a24ab52450b7344c16c1c71a558
Arvin Morawej <arvin.morawej@gmail.com>
2023-08-26 11:28:49 -0400

    fix get_user

--- src/api.rs
+++ src/api.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+use std::{future::Future, pin::Pin};
 
 use ethers::types::H160;
 use eyre::Result;
@@ -9,19 +9,25 @@ pub struct ResponseData {
     twitterUsername: String,
 }
 
-pub async fn get_user(
-    address: H160
-) -> Result<String, Box<dyn Error>> {
-    // Send the request
-    let client = reqwest::Client::new();
-    let response = client.get(format!("https://prod-api.kosetto.com/users/{:#?}", address))
-        .send()
-        .await?;
+pub fn get_user(address: H160, retry_count: usize) -> Pin<Box<dyn Future<Output = Result<String, String>>>> {
+    if retry_count == 0 {
+        return Box::pin(async { Err("Request Timeout".to_string()) });
+    }
 
-    // Deserialize the response
-    let data: ResponseData = response.json().await?;
-    println!("Twitter: @{}", data.twitterUsername);
-    Ok(data.twitterUsername)
+    Box::pin(async move {
+        let client = reqwest::Client::new();
+        let response = client.get(format!("https://prod-api.kosetto.com/users/{:#?}", address))
+            .send()
+            .await;
+
+        match response {
+            Ok(resp) => match resp.json::<ResponseData>().await {
+                Ok(data) => Ok(data.twitterUsername),
+                Err(_) => get_user(address, retry_count - 1).await,
+            },
+            Err(_) => get_user(address, retry_count - 1).await,
+        }
+    })
 }
 
 #[cfg(test)]
@@ -30,7 +36,7 @@ mod test_case {
 
     #[tokio::test]
     async fn my_stats() {
-        let result = get_user("0xBb3B8c342556De95CFF4676D0f864235Eca1128D".parse().unwrap()).await;
+        let result = get_user("0xBb3B8c342556De95CFF4676D0f864235Eca1128D".parse().unwrap(), 2).await;
 
         assert_eq!(result.unwrap(), "ThePepeology");
     }
